c0470ca8948ce515e905d4d96e02a25929c94ac6
Dell <mbikash1505@gmail.com>
2025-03-06 16:28:50 +0530

    6th March code Submit

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basic;
 public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
         System.out.println("Welcome Here");
+        System.out.println("Getting Error");
     }
 }
